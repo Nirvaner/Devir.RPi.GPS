bdf3f4096bfe0ce99219c564464318af55a289a7
Ivan <SetInNirvana@gmail.com>
2015-03-10 14:16:27 +0600

    s

--- Gps.py
+++ Gps.py
@@ -38,7 +38,7 @@ def PrintFromQueue():
 	while True:
 		if qGpsPackets.qsize() > 0:
 			p = qGpsPackets.get()
-			print("Packet-------------------------------------------------")
+			print("Packet-------------------------------------------------", end='')
 			for i in p:
 				print(i)
 			time.sleep(0.1)

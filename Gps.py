f85398f6c8419539efa7bff9090971cbb66012f2
Ivan <SetInNirvana@gmail.com>
2015-03-10 10:40:05 +0600

    optimize

--- Gps.py
+++ Gps.py
@@ -1,4 +1,19 @@
 import serial
 import time
 import struct
+import Queue
 
+qGpsPackets = Queue.Queue()
+
+gps = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0)
+
+while True:
+	try:
+		sym = gps.read()
+		if sym == "$":
+			print
+		print sym,
+	except Exception as error:
+		print error
+		pass
+		gps = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0)

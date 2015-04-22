2d2624be8b50d9064e3212dea407d899b0c88f44
Ivan <SetInNirvana@gmail.com>
2015-04-22 15:46:20 +0600

    d

--- gps.js
+++ gps.js
@@ -1,19 +1,25 @@
 var SerialPort = require("serialport").SerialPort
 var serialPort = new SerialPort("/dev/ttyAMA0");
 
-serialPort.open(function (error) {
-    if (error) {
-        console.log('Ошибка открытия порта: ' + error);
-    } else {
-        console.log('Порт открыт');
-        serialPort.on('data', function (data) {
-            console.log(data.toString());
-        });
-        serialPort.on('error', function (error) {
-            console.log('Ошибка при выполнении: ' + error);
-        });
-        serialPort.on('close', function (data) {
-            console.log('Порт был закрыт: ' + data);
-        });
-    }
+serialPort.on('data', function (data) {
+    console.log(data.toString());
 });
+serialPort.on('error', function (error) {
+    console.log('Error in runtime: ' + error);
+});
+serialPort.on('close', function (data) {
+    console.log('Port closed: ' + data);
+    Open();
+});
+
+function Open() {
+    serialPort.open(function (error) {
+        if (error) {
+            console.log('Error to open port: ' + error);
+        } else {
+            console.log('Port opened');
+        }
+    });
+}
+
+Open();

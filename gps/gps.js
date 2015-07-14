e61e3a3a6e2873ca1cabfe655c86041834aa9af7
Nirvaner <SetInNirvana@gmail.com>
2015-07-14 17:16:13 +0600

    data

--- gps/gps.js
+++ gps/gps.js
@@ -15,6 +15,7 @@ setInterval(function () {
         var lLength = Math.sqrt(lastMag.x * lastMag.x + lastMag.y * lastMag.y + lastMag.z * lastMag.z);
         var cLength = Math.sqrt(space.m.x * space.m.x + space.m.y * space.m.y + space.m.z * space.m.z);
         var a = Math.acos(scal / (lLength * cLength)) * 180 / Math.PI;
+        console.log('Angle: ', a);
 
         var distance = DistanceTo(lastCoor, gps);
 

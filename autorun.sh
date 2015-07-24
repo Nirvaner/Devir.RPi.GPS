e14967b62c7060bef8740d4386ddc46f2f1dcb4c
Nirvaner <SetInNirvaner@gmail.com>
2015-07-24 14:36:35 +0600

    done

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+
+# NAT
+iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
+
 # Run zander
-# sudo -u root -p root bash /devir/Devir.RPi.GPS/start.sh
+sudo -u root -p root bash /devir/Devir.RPi.GPS/start.sh
 
 exit 0

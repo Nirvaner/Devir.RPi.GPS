8603df12f3d3b4a06dd74fddbdacf8ffafe6ec51
Ivan <SetInNirvana@gmail.com>
2015-08-19 11:33:44 +0600

    \r

--- autorun.sh
+++ autorun.sh
@@ -1,6 +1,1 @@
-#!/usr/bin/env bash
-# NAT
-sudo -u root -p root iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
-# run Zander
-sudo -u root -p root bash /devir/Devir.RPi.GPS/start.sh
-exit 0
+#!/usr/bin/envbash#NATsudo-uroot-prootiptables-tnat-APOSTROUTING-oppp0-jMASQUERADE#runZandersudo-uroot-prootbash/devir/Devir.RPi.GPS/start.shexit0

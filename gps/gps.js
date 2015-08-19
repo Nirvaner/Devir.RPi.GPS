d6a578836e6982977c8585eb60983e7da8a44a22
Ivan <SetInNirvana@gmail.com>
2015-08-19 15:18:05 +0600

    date

--- gps/gps.js
+++ gps/gps.js
@@ -36,18 +36,18 @@ setInterval(function () {
             if (gpsQueue.length > config.MaxPackets) {
                 gpsQueue.pop();
             }
-            var buf = new Buffer(44);
+            var buf = new Buffer(46);
             buf.write(config.Imei);
             buf.writeUInt32BE(gps.date, 15);
             buf.writeFloatBE(gps.longitude, 19);
             buf.writeFloatBE(gps.latitude, 23);
-            buf.writeInt16BE(gps.altitude, 25);
-            buf.writeInt16BE(gps.angle, 27);
-            buf.writeInt16BE(gps.speed, 29);
-            buf.writeInt8(gps.satellites, 31);
-            buf.writeFloatBE(gps.pdop, 32);
-            buf.writeFloatBE(gps.hdop, 36);
-            buf.writeFloatBE(gps.vdop, 40);
+            buf.writeInt16BE(gps.altitude, 27);
+            buf.writeInt16BE(gps.angle, 29);
+            buf.writeInt16BE(gps.speed, 31);
+            buf.writeInt8(gps.satellites, 33);
+            buf.writeFloatBE(gps.pdop, 34);
+            buf.writeFloatBE(gps.hdop, 38);
+            buf.writeFloatBE(gps.vdop, 42);
             gpsQueue.unshift(buf);
         }
     }

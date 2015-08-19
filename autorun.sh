21cf45c37a94d6e46992e71a478fa66800aa0181
Ivan <SetInNirvana@gmail.com>
2015-08-19 11:36:53 +0600

    \r

--- autorun.sh
+++ autorun.sh
@@ -4,6 +4,6 @@
 sudo -u root -p root iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
 
 # run Zander
-sudo -u root -p root bash/devir/Devir.RPi.GPS/start.sh
+sudo -u root -p root bash /devir/Devir.RPi.GPS/start.sh
 
 exit 0
